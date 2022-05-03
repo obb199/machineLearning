be00cc4790e524868e1a8374bcb41ade36eb8cca
Gabriel Carvalho Santana <74666057+obb199@users.noreply.github.com>
2022-05-03 20:33:36 -0300

    Update perceptron_implementation.py

--- perceptron_implementation/perceptron_implementation.py
+++ perceptron_implementation/perceptron_implementation.py
@@ -1,31 +1,56 @@
 from math import e
-import random
+from random import randint, random
 import matplotlib.pyplot as plt
+
+def x_y_sigmoid_test():
+    x = [[2.7810836, 2.550537003, 0],
+         [1.465489372, 2.362125076, 0],
+         [3.396561688, 4.400293529, 0],
+         [1.38807019, 1.850220317, 0],
+         [3.06407232, 3.005305973, 0],
+         [7.627531214, 2.759262235, 1],
+         [5.332441248, 2.088626775, 1],
+         [6.922596716, 1.77106367, 1],
+         [8.675418651, -0.242068655, 1],
+         [7.673756466, 3.508563011, 1]]
+
+    y = [[0], [0], [0], [0], [0], [1], [1], [1], [1], [1]]
+
+    return x, y
 
 def dot_product(vec1, vec2):
     dot_result = 0
-
     for i in range(len(vec1)):
-       dot_result += vec1[i]*vec2[i]
+        dot_result += vec1[i]*vec2[i]
 
     return dot_result
 
+def relu(x):
+    if x > 0:
+        return x
+    return 0
+
+def d_relu(x):
+    if x > 0:
+        return 1
+    return 0
+
+def sigmoid(x):
+    return 1/(1 + e**(-x))
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 
 class perceptron:
-    def __init__(self, learning_rate=1e-4, actv_func='relu'):
-        self.__inputs = None
-        self.__output = None
-        self.__weights = None
-        self.__bias = random.random()
+    def __init__(self, x, y, activation_function='relu', learning_rate=0.0001):
+        self.__input = x
+        self.__output = y
+        self.__activation_function = activation_function
+
+        self.__weights = [random()*00.1 for _ in range(len(x[0]))]
+        self.__bias = random()*00.1
         self.__learning_rate = learning_rate
-        self.__actv_func = actv_func
-
-    def set_input(self, inputs):
-        self.__inputs = inputs
-        self.__weights = [random.random() for _ in range(len(self.__inputs[0]))]
-
-    def set_output(self, outputs):
-        self.__outputs = outputs
 
     def get_weights(self):
         return self.__weights
@@ -33,94 +58,34 @@ class perceptron:
     def get_bias(self):
         return self.__bias
 
-    def partial_result(self, x):
-        return dot_product(self.__weights, x) + self.__bias
+    def feedforward(self):
+        res = []
 
-    def internal_predict(self):
-        #predict de todo o conjunto de entrada
-        result = []
-        for i in self.__inputs:
-            result.append(dot_product(self.__weights, i) + self.__bias)
+        if self.__activation_function == 'relu':
+            for i in range(len(self.__input)):
+                res.append(relu(dot_product(self.__input[i], self.__weights)) + self.__bias)
+        elif self.__activation_function == 'sigmoid':
+            for i in range(len(self.__input)):
+                res.append(sigmoid(dot_product(self.__input[i], self.__weights)) + self.__bias)
 
-        return result
+        return res
 
-    def predict(self, x):
-        #predict específica de um vetor
-        return dot_product(self.__weights, x) + self.__bias
-
-    def get_sum(self):
-        sum_result = 0
-        for i in self.__inputs:
-            sum_result += dot_product(self.__weights, i) + self.__bias
-
-        return sum_result
-
-        if self.__actv_func == 'relu':
-            return self.relu(sum_result)
-        elif self.__actv_func == 'sigmoide':
-            return self.sigmoide(sum_result)
-
-    def mean_error(self):
+    def error(self):
         error = 0
-        for i in range(len(self.__inputs)):
-            error += (self.__outputs[i] - self.predict(self.__inputs[i])) * (self.__outputs[i] - self.predict(self.__inputs[i]))
+        res = self.feedforward()
 
-        return error**(0.5)/len(self.__inputs)
+        for i in range(len(self.__input)):
+            error += res[i] - self.__output[i][0]
 
-    def fit(self):
-        error = self.mean_error()
+        return error
+
+    def backpropagation(self):
+        if self.__activation_function == 'relu':
+            sum_actv = sum([d_relu(i) for i in self.feedforward()])
+        elif self.__activation_function == 'sigmoid':
+            sum_actv = sum([d_sigmoid(i) for i in self.feedforward()])
+
+        error = self.error()
         for i in range(len(self.__weights)):
-            error = self.mean_error()
-            if self.__actv_func == 'sigmoide':
-                self.__weights[i] += error * self.sigmoide(error) * self.__learning_rate
-            elif self.__actv_func == 'relu':
-                self.__weights[i] += error * self.derivative_relu(error) * self.__learning_rate
-
-        self.__bias += error * self.__learning_rate
-
-    def relu(self, x):
-        if x > 0:
-            return x
-        return 0
-
-    def derivative_relu(self, x):
-        if x > 0:
-            return 1
-        return 0
-
-    def sigmoide(self, x):
-        return 1/(1 + e**(-x))
-
-    def derivative_sigmoide(self, x):
-        sig = self.sigmoide(x)
-        return self.sigmoide(x) * (1 - self.sigmoide(x))
-
-if __name__ == '__main__':
-    # Example f(x) = 3*x + 2:
-
-    x = []
-    for i in range(-20, 20):
-        x.append([i])
-
-    y = []
-    for i in range(len(x)):
-        y.append(x[i][0]*3 + 2)
-
-    print(x)
-    print(y)
-
-    p = perceptron()
-    p.set_input(x)
-    p.set_output(y)
-
-    for i in range(25000):
-        p.fit()
-
-    res = p.internal_predict()
-
-    for i in range(len(y)):
-        print(y[i], res[i])
-
-    plt.plot(x, y)
-    plt.plot(x, res)
-    plt.show()
+            self.__weights[i] -= self.__learning_rate*error*self.__weights[i]*sum_actv
+        self.__bias -= self.__learning_rate*error*sum_actv
